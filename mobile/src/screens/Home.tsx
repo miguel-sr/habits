1bfb55743edecaf0ac3a21f4dac821dc8ff5de24
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-18 15:35:48 -0300

    :sparkles: feat: add placeholders components and "ScrollView" for summary

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,14 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+
 import { Header } from "../components/Header";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart = generateDatesFromYearBeginning();
+const minimunSummaryDatesSizes = 18 * 7;
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -19,7 +25,25 @@ export function Home() {
           </Text>
         ))}
       </View>
-      <HabitDay />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
