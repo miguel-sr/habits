c636301c3aaf724eb74b8f0b1d525d007b7f4948
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-18 13:03:13 -0300

    :sparkles: feat: create interface

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,14 @@
-import Habit from "./components/habit";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={0} />
-      <Habit completed={4} />
-      <Habit completed={6} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   );
 }
-
-export default App;
