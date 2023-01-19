8294dd47510ff46b9461bee20c7537058de96bb3
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-19 20:30:49 -0300

    :bug: fix: Button into button

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -9,14 +9,9 @@ export function Header() {
     <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
       <img src={logoImage} alt="Habits" />
       <Dialog.Root>
-        <Dialog.Trigger>
-          <button
-            type="button"
-            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
-          >
-            <Plus size={20} className="text-violet-500" />
-            Novo hábito
-          </button>
+        <Dialog.Trigger className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300">
+          <Plus size={20} className="text-violet-500" />
+          Novo hábito
         </Dialog.Trigger>
 
         <Dialog.Portal>
