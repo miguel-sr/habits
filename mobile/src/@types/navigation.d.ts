9de9711f62c5d39c3e57ddd93cea948368037d04
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-19 16:06:49 -0300

    :sparkles: feat: Add navigation to day habits

--- mobile/src/@types/navigation.d.ts
+++ mobile/src/@types/navigation.d.ts
@@ -4,7 +4,7 @@ export declare global {
       home: undefined;
       new: undefined;
       habit: {
-        data: string;
+        date: string;
       };
     }
   }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { ScrollView, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
@@ -7,10 +8,11 @@ import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const datesFromYearStart = generateDatesFromYearBeginning();
-const minimunSummaryDatesSizes = 18 * 7;
-const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length;
+const minimumSummaryDatesSizes = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -31,7 +33,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0 &&
