7fb98089494089ce303790707bfc17a3c0f2d612
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-19 16:27:03 -0300

    :sparkles: feat: Create "Habit" screen

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,5 +1,41 @@
-import { View } from "react-native";
+import dayjs from "dayjs";
+import { ScrollView, View, Text } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { BackButton } from "../components/BackButton";
+import { ProgressBar } from "../components/ProgressBar";
+import { CheckBox } from "../components/CheckBox";
+
+interface HabitParams {
+  date: string;
+}
 
 export function Habit() {
-  return <View className="flex-1 bg-background px-8 pt-16"></View>;
+  const route = useRoute();
+  const { date } = route.params as HabitParams;
+
+  const parsedDate = dayjs(date);
+  const dayOfWeek = parsedDate.format("dddd");
+  const dayAndMonnth = parsedDate.format("DD/MM");
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+
+        <Text className="text-white font-extrabold text-3xl">
+          {dayAndMonnth}
+        </Text>
+        <ProgressBar progress={75} />
+        <View className="mt-6">
+          <CheckBox title="Beber 2L de água" checked={false} />
+          <CheckBox title="Beber 2L de água" checked={true} />
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
