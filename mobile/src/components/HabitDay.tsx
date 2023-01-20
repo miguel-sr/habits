00aeaa09346ce3f0cf8bd07b47f16140674a175e
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-19 22:25:13 -0300

    :bug: fix: get summary data

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -3,6 +3,8 @@ import {
   TouchableOpacity,
   TouchableOpacityProps,
 } from "react-native";
+import clsx from "clsx";
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
@@ -10,12 +12,35 @@ export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE =
   Dimensions.get("screen").width / WEEK_DAYS - (SCREEN_HORIZONTAL_PADDING + 5);
 
-interface HabitDayProps extends TouchableOpacityProps {}
+interface HabitDayProps extends TouchableOpacityProps {
+  amount?: number;
+  completed?: number;
+  date: Date;
+}
 
-export function HabitDay({ ...rest }: HabitDayProps) {
+export function HabitDay({
+  amount = 0,
+  completed = 0,
+  date,
+  ...rest
+}: HabitDayProps) {
+  const completedPercentage =
+    amount > 0 ? generateProgressPercentage(amount, completed) : 0;
+
   return (
     <TouchableOpacity
-      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      className={clsx("rounded-lg border-2 m-1", {
+        "bg-zinc-900 border-zinc-800": completedPercentage === 0,
+        "bg-violet-900 border-violet-700":
+          completedPercentage > 0 && completedPercentage < 20,
+        "bg-violet-800 border-violet-600":
+          completedPercentage >= 20 && completedPercentage < 40,
+        "bg-violet-700 border-violet-500":
+          completedPercentage >= 40 && completedPercentage < 60,
+        "bg-violet-600 border-violet-500":
+          completedPercentage >= 60 && completedPercentage < 80,
+        "bg-violet-500 border-violet-400": completedPercentage >= 80,
+      })}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       activeOpacity={0.7}
       {...rest}
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,18 +1,53 @@
-import { ScrollView, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { Alert, ScrollView, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
+import { api } from "../lib/axios";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
 import { Header } from "../components/Header";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+import { Loading } from "../components/Loading";
+import { err } from "react-native-svg/lib/typescript/xml";
+import dayjs from "dayjs";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const datesFromYearStart = generateDatesFromYearBeginning();
-const minimumSummaryDatesSizes = 18 * 7;
+const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
+type Summary = Array<{
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}>;
+
 export function Home() {
+  const [loading, setLoading] = useState(true);
+  const [summary, setSummary] = useState<Summary>([]);
   const { navigate } = useNavigation();
+
+  async function FetchData() {
+    try {
+      setLoading(true);
+      const response = await api.get("/summary");
+      setSummary(response.data);
+    } catch (error) {
+      Alert.alert("Oops...", "Não foi possível carregar o sumário de hábitos.");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    FetchData();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -31,23 +66,35 @@ export function Home() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100 }}
       >
-        <View className="flex-row flex-wrap">
-          {datesFromYearStart.map((date) => (
-            <HabitDay
-              key={date.toISOString()}
-              onPress={() => navigate("habit", { date: date.toISOString() })}
-            />
-          ))}
+        {summary && (
+          <View className="flex-row flex-wrap">
+            {datesFromYearStart.map((date) => {
+              const dayWithHabits = summary.find((day) => {
+                return dayjs(date).isSame(day.date, "day");
+              });
 
-          {amountOfDaysToFill > 0 &&
-            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
-              <View
-                key={index}
-                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-                style={{ width: DAY_SIZE, height: DAY_SIZE }}
-              />
-            ))}
-        </View>
+              return (
+                <HabitDay
+                  key={date.toISOString()}
+                  date={date}
+                  amount={dayWithHabits?.amount}
+                  completed={dayWithHabits?.completed}
+                  onPress={() =>
+                    navigate("habit", { date: date.toISOString() })
+                  }
+                />
+              );
+            })}
+            {amountOfDaysToFill > 0 &&
+              Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+                <View
+                  key={index}
+                  className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                  style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                />
+              ))}
+          </View>
+        )}
       </ScrollView>
     </View>
   );
