9ca67c27ab10b3eb7e611fe23d3a52bc89de0c6c
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-19 22:02:18 -0300

    :bug: fix: Expo GO Request to server doesn't work

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,6 @@ app.register(cors);
 app.register(appRoutes);
 
 const port = 8089;
-app.listen({ port }).then(() => {
+app.listen({ port, host: "0.0.0.0" }).then(() => {
   console.log(`==> Server running on port ${port}!`);
 });
