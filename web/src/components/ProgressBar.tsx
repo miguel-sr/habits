d0c43dd7bfdb7e49c872be53ceb54ad6b5e07a9b
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-22 09:04:08 -0300

    :sparkles: feat: add focus properties

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -12,7 +12,7 @@ export function ProgressBar(props: ProgressBarProps) {
         role="progressbar"
         aria-label="Progresso de hábitos completados nesse dia"
         aria-valuenow={props.progress}
-        className="h-3 rounded-xl bg-violet-600"
+        className="h-3 rounded-xl bg-violet-600 transition-all duration-500"
         style={{ width: `${props.progress}%` }}
       />
     </div>
